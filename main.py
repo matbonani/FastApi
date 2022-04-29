f17ab39894d96a0cb2102c57a29bc5326c69dab7
Matheus Bonani <bonani.matheus@gmail.com>
2022-04-29 19:15:29 -0300

    users.router

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
-from company import companyapis
-from routers import auth, todos
+from routers import auth, todos, users
 from db import models
 from db.database import engine
 
@@ -9,7 +8,7 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-
+app.include_router(users.router)
 
 models.Base.metadata.create_all(bind=engine)
 
---
+++ routers/users.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
+from db import models
+from db.database import engine, SessionLocal, get_db
+from routers.auth import get_current_user, verify_password, get_password_hash
+
+router = APIRouter(
+    prefix="/user",
+    tags=["users"],
+    responses={404: {"description": "Not Found"}}
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+class UserVerification(BaseModel):
+    username: str
+    password: str
+    new_password: str
+
+
+@router.get("/")
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.UsersModel).all()
+
+
+@router.get("/user/{user_id}")
+async def user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user_model = db.query(models.UsersModel).filter(models.UsersModel.id == user_id).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user_model
+
+
+@router.get("/user/")
+async def user_by_query(user_id: int, db: Session = Depends(get_db)):
+    user_model = db.query(models.UsersModel).filter(models.UsersModel.id == user_id).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user_model
+
+
+@router.put("/password")
+async def password_change(user_verification: UserVerification, user: dict = Depends(get_current_user),
+                          db: Session = Depends(get_db)):
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_model = db.query(models.UsersModel).filter(models.UsersModel.id == user.get("id")).first()
+    if user_model is not None:
+        if user_verification.username == user_model.username and verify_password(
+                user_verification.password, user_model.password):
+            user_model.password = get_password_hash(user_verification.new_password)
+            db.add(user_model)
+            db.commit()
+            return {"message": "Password changed"}
+    return {"message": "Password is Invalid"}
+
+
+@router.delete("/user")
+async def delete_user(user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_model = db.query(models.UsersModel).filter(models.UsersModel.id == user.get("id")).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user_model)
+    db.commit()
+    return {"message": "User succsfully deleted "}
